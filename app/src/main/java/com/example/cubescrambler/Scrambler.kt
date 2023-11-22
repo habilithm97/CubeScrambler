53c8620d83a3f6baa98883fba9407e42350fd244
habilithm97 <habinkim97@gmail.com>
2023-11-22 15:55:50 +0900

    스크램블 생성(코틀린)

--- app/src/main/java/com/example/cubescrambler/Scrambler.kt
+++ app/src/main/java/com/example/cubescrambler/Scrambler.kt
@@ -1,30 +1,30 @@
-package com.example.cubescrambler;
+package com.example.cubescrambler
 
-import java.util.Random;
+import java.util.Random
 
-public class Scrambler {
-    private final static char[] Face = {'U', 'F', 'R', 'L', 'B', 'D'};
-    private final static char[] Rotation = {' ', '\'', '2'};
-    private final static Random random = new Random();
+object Scrambler {
+    private val Face = arrayOf("U", "F", "R", "L", "B", "D")
+    private val Rotation = arrayOf("", "'", "2")
+    private val random = Random()
 
-    private static String createScramble() {
-        StringBuilder scramble = new StringBuilder();
-        int length = 20;
+    private fun createScramble(): String {
+        val scramble = StringBuilder()
+        val length = 20
 
-        int face;
-        int before = -1; // 이전에 나왔던 면을 저장
-        int rotation;
+        var face: Int
+        var before = -1 // 이전에 나왔던 면을 저장
+        var rotation: Int
 
-        for(int i = 0; i < length; i++) {
+        for (i in 0 until length) {
             do {
-                face = random.nextInt(6);
-            } while(face == before || face + before == 5); // 같은 면이거나 반대 면일 경우 다시
-            before = face;
-            scramble.append(Face[face]);
+                face = random.nextInt(6)
+            } while (face == before || face + before == 5) // 같은 면이거나 반대 면일 경우 다시
+            before = face
+            scramble.append(Face[face])
 
-            rotation = random.nextInt(3);
-            scramble.append(Rotation[rotation]).append(" ");
+            rotation = random.nextInt(3)
+            scramble.append(Rotation[rotation]).append(" ")
         }
-        return scramble.toString();
+        return scramble.toString()
     }
 }
